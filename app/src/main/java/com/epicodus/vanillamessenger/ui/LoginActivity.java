20094d95caed8f79d3aab9b94366d139787f20ab
Farnoosh Johnson and Fernanda Lowe <farnoosh+fernanda@>
2016-07-13 15:03:36 -0700

    add progress dialog to login page

--- app/src/main/java/com/epicodus/vanillamessenger/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/vanillamessenger/ui/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.vanillamessenger.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +26,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
+
 
 
     @Bind(R.id.passwordLoginButton) Button mPasswordLoginButton;
@@ -57,7 +60,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         };
 
+        createAuthProgressDialog();
+
+
     }
+
+
     @Override
     public void onClick(View view) {
         if (view == mRegisterTextView) {
@@ -81,12 +89,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             mPasswordEditText.setError("Password cannot be blank");
             return;
         }
+        mAuthProgressDialog.show();
 
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
 
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
                         Log.d("TAG", "signInWithEmail:onComplete:" + task.isSuccessful());
                         if (!task.isSuccessful()) {
                             Log.w("TAG", "signInWithEmail", task.getException());
@@ -109,5 +119,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
     }
 }
