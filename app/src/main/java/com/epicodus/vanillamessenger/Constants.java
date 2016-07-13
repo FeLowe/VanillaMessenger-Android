1d326df8f078bb8c0b3ac65af27203de1f61a251
Farnoosh Johnson and Fernanda Lowe <farnoosh+fernanda@>
2016-07-13 09:54:24 -0700

    use authentication setup, make xml files for login and create account but so far no click handler working

---
+++ app/src/main/java/com/epicodus/vanillamessenger/Constants.java
@@ -0,0 +1,9 @@
+package com.epicodus.vanillamessenger;
+
+/**
+ * Created by Guest on 7/13/16.
+ */
+public class Constants {
+//
+//    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "user";
+}
