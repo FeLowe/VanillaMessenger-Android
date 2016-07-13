287b69f8ae437dabac39dc45ab487bd95876118a
Farnoosh Johnson and Fernanda Lowe <farnoosh+fernanda@>
2016-07-13 10:07:38 -0700

    add click listener for textview 'login here' to go back to login activity page.

--- app/src/main/java/com/epicodus/vanillamessenger/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/vanillamessenger/ui/CreateAccountActivity.java
@@ -1,15 +1,35 @@
 package com.epicodus.vanillamessenger.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.epicodus.vanillamessenger.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.loginTextView)
+    TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        ButterKnife.bind(this);
+        mLoginTextView.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if (view == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
