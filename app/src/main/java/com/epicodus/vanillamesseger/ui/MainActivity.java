a9ed83d55345f913f11d82e4d79906d76f38c061
Farnoosh Johnson and Fernanda Lowe <farnoosh+fernanda@>
2016-07-13 09:02:19 -0700

    create all files and folders to initiate project(aka fisrt sync), and create packages

---
+++ app/src/main/java/com/epicodus/vanillamesseger/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.vanillamesseger.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.vanillamesseger.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
